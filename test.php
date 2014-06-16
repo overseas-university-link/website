12a18a45ac301be1eb83d31f60056707d99551ff
HP Envy <HP Envy@192.168.1.147>
2014-06-16 13:50:24 +0300

    Initial Commit with skeleton of external website

---
+++ test.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>About | Overseas University Link</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="http://localhost/oul/xmlrpc.php">
+	<!--[if lt IE 9]>
+	<script src="http://localhost/oul/wp-content/themes/custom/js/html5.js"></script>
+	<![endif]-->
+	<link rel="alternate" type="application/rss+xml" title="Overseas University Link &raquo; Feed" href="http://localhost/oul/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Overseas University Link &raquo; Comments Feed" href="http://localhost/oul/?feed=comments-rss2" />
+<link rel="alternate" type="application/rss+xml" title="Overseas University Link &raquo; About Comments Feed" href="http://localhost/oul/?feed=rss2&#038;page_id=4" />
+<link rel='stylesheet' id='twentyfourteen-lato-css'  href='//fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C900%2C300italic%2C400italic%2C700italic' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='http://localhost/oul/wp-content/themes/custom/genericons/genericons.css?ver=3.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='twentyfourteen-style-css'  href='http://localhost/oul/wp-content/themes/custom/style.css?ver=3.9.1' type='text/css' media='all' />
+<!--[if lt IE 9]>
+<link rel='stylesheet' id='twentyfourteen-ie-css'  href='http://localhost/oul/wp-content/themes/custom/css/ie.css?ver=20131205' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='http://localhost/oul/wp-includes/js/jquery/jquery.js?ver=1.11.0'></script>
+<script type='text/javascript' src='http://localhost/oul/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost/oul/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost/oul/wp-includes/wlwmanifest.xml" /> 
+<link rel='next' title='Home' href='http://localhost/oul/' />
+<meta name="generator" content="WordPress 3.9.1" />
+<link rel='canonical' href='http://localhost/oul/?page_id=4' />
+<link rel='shortlink' href='http://localhost/oul/?p=4' />
+	<style type="text/css" id="twentyfourteen-header-css">
+			.site-title a {
+			color: #dd7e1f;
+		}
+		</style>
+	<style type="text/css" id="custom-background-css">
+body.custom-background { background-color: #919191; background-image: url('http://localhost/oul/wp-content/uploads/2014/06/dotted.jpg'); background-repeat: repeat; background-position: top center; background-attachment: scroll; }
+</style>
+</head>
+
+<body style=" margin-left: auto;
+    margin-right: auto;
+    width: 80%;" class="page page-id-4 page-template-default custom-background masthead-fixed singular">
+<div id="page" class="hfeed site">
+	
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">
+			<h1 class="site-title"><a href="http://localhost/oul/" rel="home">Overseas University Link</a></h1>
+
+			<div class="search-toggle">
+				<a href="#search-container" class="screen-reader-text">Search</a>
+			</div>
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle">Primary Menu</button>
+				<a class="screen-reader-text skip-link" href="#content">Skip to content</a>
+				<div class="nav-menu"><ul><li class="page_item page-item-6"><a href="http://localhost/oul/">Home</a></li><li class="page_item page-item-4 current_page_item"><a href="http://localhost/oul/?page_id=4">About</a></li><li class="page_item page-item-11"><a href="http://localhost/oul/?page_id=11">University Destinations</a></li><li class="page_item page-item-13"><a href="http://localhost/oul/?page_id=13">Services</a></li><li class="page_item page-item-15"><a href="http://localhost/oul/?page_id=15">Scholarships</a></li><li class="page_item page-item-17"><a href="http://localhost/oul/?page_id=17">FAQs</a></li><li class="page_item page-item-19"><a href="http://localhost/oul/?page_id=19">Contact us</a></li></ul></div>
+			</nav>
+		</div>
+
+		<div id="search-container" class="search-box-wrapper hide">
+			<div class="search-box">
+				<form role="search" method="get" class="search-form" action="http://localhost/oul/">
+				<label>
+					<span class="screen-reader-text">Search for:</span>
+					<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" title="Search for:" />
+				</label>
+				<input type="submit" class="search-submit" value="Search" />
+			</form>			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
+
+<div id="main-content" class="main-content">
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			
+<article id="post-4" class="post-4 page type-page status-publish hentry">
+	<header class="entry-header"><h1 class="entry-title">About</h1></header><!-- .entry-header -->
+	<div class="entry-content">
+		<p>Overseas University Link is a company that was established in 2014 to enable Ugandan students access quality overseas education. The goal is top bridge then gap between Ugandans and Education overseas.</p>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
+
+<div id="comments" class="comments-area">
+
+	
+									<div id="respond" class="comment-respond">
+				<h3 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="/oul/?page_id=4#respond" style="display:none;">Cancel reply</a></small></h3>
+									<form action="http://localhost/oul/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate>
+																			<p class="comment-notes">Your email address will not be published. Required fields are marked <span class="required">*</span></p>							<p class="comment-form-author"><label for="author">Name <span class="required">*</span></label> <input id="author" name="author" type="text" value="" size="30" aria-required='true' /></p>
+<p class="comment-form-email"><label for="email">Email <span class="required">*</span></label> <input id="email" name="email" type="email" value="" size="30" aria-required='true' /></p>
+<p class="comment-form-url"><label for="url">Website</label> <input id="url" name="url" type="url" value="" size="30" /></p>
+												<p class="comment-form-comment"><label for="comment">Comment</label> <textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>						<p class="form-allowed-tags">You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:  <code>&lt;a href=&quot;&quot; title=&quot;&quot;&gt; &lt;abbr title=&quot;&quot;&gt; &lt;acronym title=&quot;&quot;&gt; &lt;b&gt; &lt;blockquote cite=&quot;&quot;&gt; &lt;cite&gt; &lt;code&gt; &lt;del datetime=&quot;&quot;&gt; &lt;em&gt; &lt;i&gt; &lt;q cite=&quot;&quot;&gt; &lt;strike&gt; &lt;strong&gt; </code></p>						<p class="form-submit">
+							<input name="submit" type="submit" id="submit" value="Post Comment" />
+							<input type='hidden' name='comment_post_ID' value='4' id='comment_post_ID' />
+<input type='hidden' name='comment_parent' id='comment_parent' value='0' />
+						</p>
+											</form>
+							</div><!-- #respond -->
+			
+</div><!-- #comments -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	</div><!-- #main-content -->
+
+<div id="secondary">
+		<h2 class="site-description">Getting you there</h2>
+	
+	
+	</div><!-- #secondary -->
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			
+			<div class="site-info">
+								<a href="http://wordpress.org/">Proudly powered by WordPress</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<script type='text/javascript' src='http://localhost/oul/wp-includes/js/comment-reply.min.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://localhost/oul/wp-content/themes/custom/js/functions.js?ver=20140319'></script>
+</body>
+</html>
---
+++ wp-content/themes/twentytwelve/footer.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+            <div style="width: 100%;">
+	<table  width="100%">
+			<td>
+			<b>Countries</b> <br/>
+				<ul>
+					<li>England</li>
+					<li>Wales</li>
+					<li>Scotland</li>
+				</ul>
+			</td>
+		
+		<td>
+			<b>Social Media</b> <br/>
+				<ul>
+					<li>Facebook</li>
+					<li>Google Plus</li>
+					<li>Twitter</li>
+				</ul>
+			</td>
+		
+		<td>
+			<b>Popular Courses</b> <br/>
+				<ul>
+					<li>Law</li>
+					<li>Computer Science</li>
+					<li>Medicine</li>
+				</ul>
+			</td>
+			<td>
+			<b>Office Location</b> <br/>
+				Overseas University Link<br/>
+				Akamwesi Complex<br/>
+				Plot 121 Bunyonyi Drive, Nakawa<br>
+				P.O Box 10975 Kampala - Uganda
+			</td>
+		</tr>
+	</table>
+			
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
